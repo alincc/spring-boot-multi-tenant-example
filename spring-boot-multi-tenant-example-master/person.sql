0fbd7b54bf76e0018c41cb80b0fb18f808c8ed15
qaistars <qaistars@gmail.com>
2017-07-21 11:03:34 -0500

    Add files via upload

---
+++ spring-boot-multi-tenant-example-master/person.sql
@@ -0,0 +1,11 @@
+CREATE TABLE schema1.person (
+    id bigserial NOT NULL PRIMARY KEY,
+    first_name character varying,
+    last_name character varying
+);
+
+CREATE TABLE schema2.person (
+    id bigserial NOT NULL PRIMARY KEY,
+    first_name character varying,
+    last_name character varying
+);
---
+++ spring-boot-multi-tenant-example-master/src/main/java/com/gdb/Application.java
@@ -0,0 +1,33 @@
+package com.gdb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.gdb.config.MultiTenantFilter;
+
+import javax.servlet.Filter;
+
+@SpringBootApplication
+public class Application {
+
+    @Autowired
+    AutowireCapableBeanFactory beanFactory;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean myFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        Filter tenantFilter = new MultiTenantFilter();
+        beanFactory.autowireBean(tenantFilter);
+        registration.setFilter(tenantFilter);
+        registration.addUrlPatterns("/*");
+        return registration;
+    }
+}
